4af8814e9b18b6918fc69a04d5b258bf5d56971c
vsantos1 <vinicius.shyn2@gmail.com>
2023-01-06 22:22:49 -0300

    feat: payment model/repository

---
+++ src/main/java/com/vsantos1/banking/models/Payment.java
@@ -0,0 +1,73 @@
+package com.vsantos1.banking.models;
+
+
+import com.vsantos1.banking.enums.PaymentStatus;
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_payments")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    private Integer paymentStatus;
+
+    private String description;
+
+    private Float amount;
+
+    private Date createdAt;
+
+
+    public Payment() {
+    }
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Float amount) {
+        this.amount = amount;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public PaymentStatus getPaymentStatus() {
+        return PaymentStatus.valueOf(paymentStatus);
+    }
+
+    public void setPaymentStatus(PaymentStatus paymentStatus) {
+        if (paymentStatus != null) {
+            this.paymentStatus = paymentStatus.getCode();
+        }
+
+    }
+}
+
---
+++ src/main/java/com/vsantos1/banking/repositories/PaymentRepository.java
@@ -0,0 +1,11 @@
+package com.vsantos1.banking.repositories;
+
+import com.vsantos1.banking.models.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, UUID> {
+}
