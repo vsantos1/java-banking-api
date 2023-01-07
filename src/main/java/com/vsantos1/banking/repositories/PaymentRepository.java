f3e1f78f9b6c7bbf9e92b1268cc3b02e918df138
vsantos1 <vinicius.shyn2@gmail.com>
2023-01-06 22:46:30 -0300

    feat: payment service

--- src/main/java/com/vsantos1/banking/repositories/PaymentRepository.java
+++ src/main/java/com/vsantos1/banking/repositories/PaymentRepository.java
@@ -1,6 +1,8 @@
 package com.vsantos1.banking.repositories;
 
 import com.vsantos1.banking.models.Payment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import java.util.UUID;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
+
+    Page<Payment> findAllByPaymentStatus(Integer paymentStatus, Pageable pageable);
 }
--- src/main/java/com/vsantos1/banking/services/PaymentService.java
+++ src/main/java/com/vsantos1/banking/services/PaymentService.java
@@ -1,7 +1,17 @@
 package com.vsantos1.banking.services;
 
+import com.vsantos1.banking.mapper.MapperUtils;
+import com.vsantos1.banking.models.Payment;
 import com.vsantos1.banking.repositories.PaymentRepository;
+import com.vsantos1.banking.vo.PaymentVO;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class PaymentService {
@@ -10,6 +20,48 @@ public class PaymentService {
 
     public PaymentService(PaymentRepository paymentRepository) {
         this.paymentRepository = paymentRepository;
+    }
+
+    public Page<PaymentVO> findAll(Pageable pageable) {
+        Page<Payment> payments = paymentRepository.findAll(pageable);
+
+
+        return payments.map(payment -> MapperUtils.parseObject(payment, PaymentVO.class));
+    }
+
+    public Page<PaymentVO> findAllByPaymentStatus(Integer paymentStatus, Pageable pageable) {
+        Page<Payment> payments = paymentRepository.findAllByPaymentStatus(paymentStatus, pageable);
+
+        return payments.map(payment -> MapperUtils.parseObject(payment, PaymentVO.class));
+    }
+    @Transactional
+    public PaymentVO save(Payment payment) {
+        /* SAVE AND PARSE THE PAYMENT MODEL TO VALUE OBJECT */
+        return MapperUtils.parseObject(paymentRepository.save(payment), PaymentVO.class);
+    }
+
+
+    public PaymentVO findById(UUID id) {
+        /* FIND BY ID AND PARSE THE PAYMENT MODEL TO VALUE OBJECT */
+        Optional<Payment> paymentOptional = paymentRepository.findById(id);
+        if (paymentOptional.isEmpty()) {
+            throw new ResourceNotFoundException("No records found for this ID");
+        }
+
+        return MapperUtils.parseObject(paymentOptional, PaymentVO.class);
+
+    }
+
+
+    @Transactional
+    public void delete(UUID id) {
+        /* FIND BY ID AND DELETE THE PAYMENT MODEL */
+        Optional<Payment> paymentOptional = paymentRepository.findById(id);
+        if (paymentOptional.isEmpty()) {
+            throw new ResourceNotFoundException("No records found for this ID");
+        }
+
+        paymentRepository.delete(paymentOptional.get());
     }
 
 }
