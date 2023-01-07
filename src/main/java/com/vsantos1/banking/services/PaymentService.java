2371ee3027fc3058769c8ca9634de9c7a947d09a
vsantos1 <vinicius.shyn2@gmail.com>
2023-01-06 22:23:58 -0300

    feat: payment service

---
+++ src/main/java/com/vsantos1/banking/services/PaymentService.java
@@ -0,0 +1,15 @@
+package com.vsantos1.banking.services;
+
+import com.vsantos1.banking.repositories.PaymentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    private final PaymentRepository paymentRepository;
+
+    public PaymentService(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
+}
