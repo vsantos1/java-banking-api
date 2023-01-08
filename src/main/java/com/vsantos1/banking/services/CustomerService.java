ffe51e8ba4fdedafcfc9049910c2e51d8866a5e0
vsantos1 <vinicius.shyn2@gmail.com>
2023-01-08 20:41:10 -0300

    feat: customer service

---
+++ src/main/java/com/vsantos1/banking/services/CustomerService.java
@@ -0,0 +1,44 @@
+package com.vsantos1.banking.services;
+
+import com.vsantos1.banking.mapper.MapperUtils;
+import com.vsantos1.banking.models.Customer;
+import com.vsantos1.banking.repositories.CustomerRepository;
+import com.vsantos1.banking.vo.CustomerVO;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+
+    public boolean isEmailAlreadyRegistered(String email) {
+        return customerRepository.findByEmail(email).isPresent();
+    }
+
+    public boolean isCpfAlreadyRegistered(String cpf) {
+        return customerRepository.findByCpf(cpf).isPresent();
+    }
+
+    @Transactional
+    public CustomerVO save(Customer customer) {
+
+       return MapperUtils.parseObject(customerRepository.save(customer), CustomerVO.class);
+    }
+
+    public Optional<CustomerVO> findById(Long id) {
+        return customerRepository.findById(id).map(customer -> MapperUtils.parseObject(customer, CustomerVO.class));
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        customerRepository.deleteById(id);
+    }
+}
